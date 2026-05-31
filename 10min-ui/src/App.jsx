743899981d1905f3b6a16a49e7b24cb9e7f261b1
hina0629 <hina.0629.xxx@gmail.com>
2026-05-31 23:55:05 +0900

    feat: 20260531 線の上を別の線が伸びる

--- 10min-ui/src/App.jsx
+++ 10min-ui/src/App.jsx
@@ -1,8 +1,10 @@
 import Day1 from "./components/20260530/Day1"
+import Day2 from "./components/20260531/Day2"
 
 function App() {
   return (<>
         <Day1 />
+        <Day2 />
     </>)
 }
 
---
+++ 10min-ui/src/components/20260531/Day2.jsx
@@ -0,0 +1,15 @@
+import styles from './Day2.module.css'
+
+function Day1() {
+  return (
+    <div>
+      <h2>Day2</h2>
+
+      <a href="#" className={styles.btn}>
+        <span>線の上を別の線が伸びる</span>
+      </a>
+    </div>
+  )
+}
+
+export default Day1
