f9a3eab07041d3339955db7a36a6f6bcfba92b60
hina0629 <hina.0629.xxx@gmail.com>
2026-06-02 22:05:25 +0900

    feat: 20260602 おしゃれなステップ矢印

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Day1 from "./components/20260530/Day1"
 import Day2 from "./components/20260531/Day2"
 import Day3 from "./components/20260601/Day3"
+import Day4 from "./components/20260602/Day4"
 
 function App() {
   return (<>
         <Day1 />
         <Day2 />
         <Day3 />
+        <Day4 />
     </>)
 }
 
---
+++ src/components/20260602/Day4.jsx
@@ -0,0 +1,23 @@
+import styles from './Day4.module.css'
+
+function Day4() {
+  return (
+    <div>
+      <h2>Day4</h2>
+      <div className={styles.wrapper}>
+        <ul className={styles.ul}>
+          <li></li>
+          <li><span></span></li>
+          <li><span></span></li>
+        </ul>
+        <ul className={styles.ul_content}>
+          <li><div></div></li>
+          <li><div></div></li>
+          <li><div></div></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Day4
