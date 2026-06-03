28242ff63962cbe12848d3b0344bd75c47bb452f
hina0629 <hina.0629.xxx@gmail.com>
2026-06-03 23:46:55 +0900

    feat: 20260603 矢印移動と背景ボタン

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Day1 from "./components/20260530/Day1"
 import Day2 from "./components/20260531/Day2"
 import Day3 from "./components/20260601/Day3"
 import Day4 from "./components/20260602/Day4"
+import Day5 from "./components/20260603/Day5"
 
 function App() {
   return (<>
@@ -9,6 +10,7 @@ function App() {
         <Day2 />
         <Day3 />
         <Day4 />
+        <Day5 />
     </>)
 }
 
---
+++ src/components/20260603/Day5.jsx
@@ -0,0 +1,12 @@
+import styles from './Day5.module.css'
+
+function Day5() {
+  return (
+    <div>
+      <h2>Day5</h2>
+      <a href="#" className={styles.btn}>矢印移動と背景</a>
+    </div>
+  )
+}
+
+export default Day5
